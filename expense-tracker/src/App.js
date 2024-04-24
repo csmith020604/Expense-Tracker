8de0d57fb8caa0c641809157e9aff9e3dd4f3751
Colby <colbysmithinformation@gmail.com>
2024-04-24 00:21:26 -0500

    inital commit/setup

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This will be a mobile expense tracker. It will be static but responsive for all devices
+    </div>
+  );
+}
+
+export default App;
